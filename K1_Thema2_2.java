1f03e057143280e247130742f85606f49848d7c3
fuqthislag <fuqthislag@users.noreply.github.com>
2020-09-08 16:45:49 +0000

    Final

---
+++ K1_Thema2_2.java
@@ -0,0 +1,15 @@
+class K1_Thema2_2 {
+    public static void main(String[] args) {
+        System.out.println(se(2,3));
+        System.out.println(se(2,2));
+        System.out.println(se(2,1));
+        System.out.println(se(2,0));
+    }
+
+    public static double se(double x, double y) {
+        if (y == 0)
+            return 1;
+        else
+            return Math.pow(x, se(x, y - 1));
+    }
+}
